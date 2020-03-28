5b00b12a7a0dd0af57644a7e32132e4c1fa2650e
tomoaki.iwamoto <info@tokiwakanpou.com>
2020-03-28 13:55:42 +0900

    Initial Commit

---
+++ SecoundKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecoundKadaiApp
+//
+//  Created by 岩本知章 on 2020/03/28.
+//  Copyright © 2020 tomoaki.iwamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
